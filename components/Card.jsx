37eafab4711d3bc60cf69f93b0c8014d44eee5f1
Precious Olusola <preciousolusola16@gmail.com>
2023-01-15 14:19:20 +0100

    Added Jobs card

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,16 +1,24 @@
 import { StyleSheet } from 'react-native';
 import { View, Text } from 'react-native-ui-lib';
-
+import Theme from '../constants/theme';
 import React from 'react'
 
-const Card = () => {
+const Card = ({children}) => {
   return (
-    <View>
-      <Text>A Card content</Text>
+    <View style={styles.cardContainer}>
+      <Text>Sample card content</Text>
     </View>
   )
 }
 
 export default Card
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    cardContainer:{
+        backgroundColor:Theme.white,
+        marginHorizontal: 20,
+        marginVertical: 10,
+        padding: 10,
+        borderRadius: 10,
+    }
+})
