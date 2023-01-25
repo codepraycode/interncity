e761b3b694ad1d18667e966737f216f5cdd22873
Precious Olusola <preciousolusola16@gmail.com>
2023-01-25 01:51:43 +0100

    Added the interns list screen

--- features/Interns/InternsList.jsx
+++ features/Interns/InternsList.jsx
@@ -1,17 +1,45 @@
 import React from 'react'
-import { View, Text} from 'react-native-ui-lib';
-import { StyleSheet } from 'react-native';
+import { View, Text, Image} from 'react-native-ui-lib';
+import { FlatList, StyleSheet } from 'react-native';
+import { InternLists } from '../../constants/dummy';
+import assets from '../../constants/assets';
+import Card from '../../components/Card';
 
+
+
+const InternItem = ({data, onViewClick})=>{
+    const {firstname, lastname} = data;
+
+    // const name = `${firstname}`
+
+    return (
+        <Card>
+            
+            <View style={{flexDirection:'row', alignItems:'center', marginVertical:5,}}>
+
+                <Image
+                    source={assets.user}
+                    resizeMode="cover"
+                />
+
+                <View style={{marginLeft:20,}}>
+                <Text h4>{firstname} {lastname}</Text>
+                <Text small>Federal University of Technology Akure</Text>
+                </View>
+            </View>
+        </Card>
+    )
+}
 
 const InternsListScreen = () => {
 
     // console.log(dateIntervals);
     return (
-        <View flex center>
-
-            <Text>Inters screen</Text>
-            
-        </View>
+        <FlatList
+            data={ InternLists }
+            renderItem = {({item})=><InternItem data = { item} onViewClick = {()=>{}}/>}
+            keyExtractor={item => item.id}
+        />
     )
 }
 
