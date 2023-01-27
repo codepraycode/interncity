94132c1146f0b5c7c48d0c785ff2056b2ad2fa11
Precious Olusola <preciousolusola16@gmail.com>
2023-01-27 16:41:49 +0100

    Added snippets for CRUD operations on user profile

--- features/Profile/ProfileCreation.jsx
+++ features/Profile/ProfileCreation.jsx
@@ -1,16 +1,26 @@
-import React, { useContext, useState } from 'react';
-import { ScrollView, StyleSheet,TouchableOpacity } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Text, View } from 'react-native-ui-lib';
+import React, { useContext, useEffect, useState } from 'react';
+import { StyleSheet, } from 'react-native';
+import { View } from 'react-native-ui-lib';
 import AppContext from '../../app/context';
 import Form from '../../components/form';
 import Theme from '../../constants/theme';
 import { UserAccount } from '../../app/models/User.js'
 import SafeAreaLayout from '../../components/Layout';
+import { database, collectionNames} from '../../app/firebaseConfig';
+import { 
+  collection, 
+  addDoc, 
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc
+} from 'firebase/firestore';
+import { JSONLog } from '../../app/utils';
 
 const ProfileFormScreen = ({navigation, route}) =>{
-    const {updateProfile} = useContext(AppContext);
+    const userProfileCollectionRef = collection(database,collectionNames.USER_PROFILE);
 
+    const {updateProfile} = useContext(AppContext);
     const {profileType} = route.params;
       
     const [formErrors, setFormErrors] = useState({});
@@ -19,9 +29,63 @@ const ProfileFormScreen = ({navigation, route}) =>{
     const handleCreateProfile = (data)=>{
       if (loading) return;
       console.log("create profile",data);
+
+      addDoc(userProfileCollectionRef,{
+        name:"Sample name",
+        type: profileType,
+      })
+      .then(()=>{
+        console.log("Created document")
+      })
+      .catch((err)=>{
+        console.log("Error creating document")
+        console.log(err);
+      })
       // updateProfile({dt:"sample data"});
       // navigation.navigate("ProfileSuccess");
     }
+
+    const getAllDocs = ()=>{
+      getDocs(userProfileCollectionRef)
+      .then((res)=>{
+        JSONLog(res.docs.map(item=> ({...item.data(), id:item.id})));
+      })
+      .catch(err=>{
+        console.log("getDocs error:", err);
+      })
+    }
+
+    const updateDocs = ()=>{
+      const id = "1NOSUypqidOFc9OJcESp";
+      const docToUpdate = doc(database, collectionNames.USER_PROFILE, id);
+
+      updateDoc(docToUpdate, {
+        email:"Abs@maile.com"
+      })
+      .then(()=>{
+        console.log("Data updated")
+      })
+      .catch(err=>{
+        console.log(err.message);
+      })
+    }
+
+    const deleteADoc = ()=>{
+      const id = "1NOSUypqidOFc9OJcESp";
+      const docToDelete = doc(database, collectionNames.USER_PROFILE, id);
+
+      deleteDoc(docToDelete)
+      .then(()=>{
+        console.log("Data deleted")
+      })
+      .catch(err=>{
+        console.log(err.message);
+      })
+    }
+
+    useEffect(()=>{
+      
+    })
 
     const formSchema = UserAccount.getProfileSchema(profileType);
 
