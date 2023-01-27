3af1f5784d22c827c06e9fe4be352fe5d3c2a7e4
Precious Olusola <preciousolusola16@gmail.com>
2023-01-27 16:00:16 +0100

    Creating a new branch from this point

--- app/context.jsx
+++ app/context.jsx
@@ -8,12 +8,13 @@ export default AppContext;
 export const AppContextProvider = ({children})=>{
 
     const initialState = {
-        userAccount: {
-            name:"Smaple name",
-            token:{
-                access:"dfakslkjflask"
-            }
-        },
+        userAccount: null,
+        // {
+        //     name:"Smaple name",
+        //     token:{
+        //         access:"dfakslkjflask"
+        //     }
+        // },
         userProfile: null,
         userType:'supervisor'//"organization"
     }
