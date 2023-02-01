d8e29a1bd1e1c2cd389412ff5a4bdfa54ec926a1
Precious Olusola <preciousolusola16@gmail.com>
2023-02-01 03:35:25 +0100

    Resolved async storage issue

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,5 +1,10 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import {
+  initializeAuth,
+  getReactNativePersistence
+} from 'firebase/auth/react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Store in env for production
 const configurations = {
@@ -14,6 +19,11 @@ const configurations = {
 
 export const app = initializeApp(configurations);
 export const database = getFirestore(app);
+// initialize auth
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+
 
 export const collectionNames = {
      USER_ACCOUNT:"userAccounts",
