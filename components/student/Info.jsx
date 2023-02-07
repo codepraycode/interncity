1c105ff7a9427900f6e665a8099f1b61b48970f3
Precious Olusola <preciousolusola16@gmail.com>
2023-02-07 01:32:10 +0100

    Updated info component

--- components/student/Info.jsx
+++ components/student/Info.jsx
@@ -1,12 +1,48 @@
 import { View, Text } from 'react-native-ui-lib';
 import React from 'react'
 
-const Item = () => {
+const Info = () => {
   return (
-    <View>
-      <Text>Item</Text>
+    <View
+        style={{
+            paddingVertical: 10,
+            marginHorizontal: 30,
+        }}
+    >
+        <View style={{marginVertical: 10}}>
+            <Text p style={{marginVertical: 10}}>Phone number</Text>
+
+            <Text h6>
+                +234 8000000000
+            </Text>
+        </View>
+
+        <View style={{marginVertical: 10}}>
+            <Text p style={{marginVertical: 10}}>Address</Text>
+
+            <Text h6 style={{marginVertical: 0}}>
+                Ota, Lagos.
+            </Text>
+        </View>
+
+        <View style={{marginVertical: 10}}>
+            <Text p style={{marginVertical: 10}}>Department</Text>
+
+            <Text h6 style={{marginVertical: 0}}>
+                Cyber security
+            </Text>
+        </View>
+
+        <View style={{marginVertical: 10}}>
+            <Text p style={{marginVertical: 10}}>School</Text>
+
+            <Text h6 style={{marginVertical: 0}}>
+                Federal University of Technology Akure
+            </Text>
+        </View>
+
     </View>
   )
 }
 
-export default Item
+export default Info
