af7c0c2edb86ff1320dcfac8618eab36de3c29fc
Precious Olusola <preciousolusola16@gmail.com>
2023-02-07 01:51:18 +0100

    Updated requirements in organization interns

--- components/student/Item.jsx
+++ components/student/Item.jsx
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native-ui-lib';
+import { View, Text, Image } from 'react-native-ui-lib';
 import React from 'react'
+import Card from '../Card';
+import assets from '../../constants/assets';
+import Seperator from '../Seperator';
 
-const Item = () => {
-  return (
-    <View>
-      <Text>Item</Text>
-    </View>
-  )
+const Item = ({data, onViewClick}) => {
+    const {firstname, lastname} = data;
+
+    return (
+        <Card clickable={true} onPress={onViewClick}>
+            
+            <View style={{flexDirection:'row', alignItems:'center', marginVertical:5,}}>
+
+                <Image
+                    source={assets.user}
+                    resizeMode="cover"
+                />
+
+                <View style={{marginLeft:20,}}>
+                <Text h4>{firstname} {lastname}</Text>
+                <View style={{flexDirection:'row', 'alignItems':'center'}}>
+                    <Text small>FUTA</Text>
+                    <Seperator/>
+                    <Text small>Cyber security</Text>
+                </View>
+                </View>
+            </View>
+        </Card>
+    )
 }
 
-export default Item
+export default Item;
