f8f9d8b1f9c7d83536098134051636d3f9a42aa4
Precious Olusola <preciousolusola16@gmail.com>
2023-02-07 01:45:19 +0100

    Updated student detail

---
+++ components/student/Log.jsx
@@ -0,0 +1,84 @@
+import { View, Text, } from 'react-native-ui-lib';
+import React from 'react'
+import { weeksBetween } from '../../app/utils';
+
+import { FlatList, TouchableOpacity } from 'react-native';
+import Theme from '../../constants/theme';
+
+
+const LogItem = ({label, log, editLog}) => {
+
+    const breakLength = 178;
+    
+    return (
+        <TouchableOpacity 
+            activeOpacity={0.4} 
+            onPress={editLog}
+            style={{
+                backgroundColor:Theme.white,                
+                padding: 10,
+                marginVertical:10,
+                marginHorizontal:20,
+                height: 120,
+                width: 350,
+                maxWidth: "90%",
+                borderRadius: 4,
+
+                overflow:'hidden',
+            }}
+        >
+            {/* Title */}
+            <Text label style={{
+                marginBottom: 10,
+            }}>
+                {label}
+            </Text>
+
+            {/* Content */}
+            <View style={{ height: "100%"}}>
+
+                {
+                    (!log || log.length <1) ?
+                    <Text i center style={{color: Theme.grey300, fontSize: 18}}>
+                        No log yet
+                    </Text>
+                    :
+                    <Text p style={{paddingHorizontal: 10,}}>
+                        {log.substring(0,breakLength) + (log.length > breakLength ? '...':'')}
+                    </Text>
+                }
+                
+            
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+export const WeeklyLogs = ({onEditLog}) => {
+    
+    const realDate = new Date('2022-01-02');
+
+    const numberOfWeeks = weeksBetween(realDate);
+
+    const weeks = [...Array(numberOfWeeks).keys()];
+    const log = `Date: 1/1/2023
+
+A sample weekly log.
+
+supervisor: Mr Lorem Bulaba (Manager)
+`
+    return (
+        <FlatList
+            data={weeks}
+            renderItem = {({item})=>(
+                <LogItem 
+                    editLog={()=>onEditLog(item+1)} 
+                    label={`Week ${item+1}`}
+                    log={log}
+                />
+            )}
+        />
+    )
+}
+
+export default LogItem;
---
+++ features/Students/StudentDetail.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import { StyleSheet } from 'react-native';
+import { View } from 'react-native-ui-lib';
+import Theme from '../../constants/theme';
+import { InternLists, JobsLists } from '../../constants/dummy';
+import JobNotFound from '../../states/JobNotFound';
+
+import InternWeekelyReview from './InternWeeklyReview';
+import { LogBottomSheet } from '../../components/BottomSheet';
+import Tabs from '../../components/Tabs';
+import DetailHeader from '../../components/student/Detail';
+import Info from '../../components/student/Info';
+
+
+const InternsDetailScreen = ({ route }) => {
+    const { internId} = route.params;
+    const internData = InternLists.find(each => each.id === internId);
+
+    const [tabNo, setTabNo] = useState(0);    
+    const [weekEditing, setWeekEditing] = useState(null);
+
+
+    const autoSaveLog = (data)=>{
+
+        setWeekEditing(null);
+    }
+
+    if (!Boolean(internData)) return <JobNotFound/>;
+
+    return (
+        <>
+            <View 
+                contentContainerStyle={{
+                    backgroundColor:Theme.grey100,
+                }}
+            >
+
+                <DetailHeader data = { internData }/>
+
+                {/* Tabs */}
+                <View 
+                    centerH
+                    style={{
+                        marginBottom: 10,
+                    }}
+                >
+                    <Tabs 
+                        tabs={[
+                            {
+                                text: "Intern Info",
+                                onClick:()=>setTabNo(0),
+                                active: tabNo === 0
+                            },
+                            {
+                                text: "Weekly Reviews",
+                                onClick:()=>setTabNo(1),
+                                active: tabNo === 1
+                            }
+                        ]}
+                    />
+                </View>
+            </View>
+
+            {/* Content */}
+                
+            {
+                tabNo === 0 ? 
+                <Info/>
+                :
+                <InternWeekelyReview onEditLog={(weekNumber)=>setWeekEditing(weekNumber)}/>
+            }
+
+            <LogBottomSheet weekly={true} show={Boolean(weekEditing)} data={weekEditing} onDismiss={autoSaveLog}/>
+        </>
+
+    );
+}
+
+export default InternsDetailScreen;
+
+
+
+const styles = StyleSheet.create({})
