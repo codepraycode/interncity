527b94b6500960731f1513a37b97c02605e534db
Precious Olusola <preciousolusola16@gmail.com>
2023-02-07 01:42:45 +0100

    Updated Intern detail screen

--- features/Interns/InternDetail.jsx
+++ features/Interns/InternDetail.jsx
@@ -1,117 +1,21 @@
 import React, { useState } from 'react'
-import { Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { View, Text, } from 'react-native-ui-lib';
+import { View } from 'react-native-ui-lib';
 import Theme from '../../constants/theme';
-import { InternLists, JobsLists } from '../../constants/dummy';
+import { InternLists } from '../../constants/dummy';
 import JobNotFound from '../../states/JobNotFound';
 
-import Octicons from 'react-native-vector-icons/Octicons';
-import assets from '../../constants/assets';
-import InternInfo from './InternInfo';
-import InternWeekelyReview from './InternWeeklyReview';
 import { LogBottomSheet } from '../../components/BottomSheet';
-
-const InternDetailHeader = ({data})=>{
-
-    const name = `Lorem Ipsum`;
-    const school = "Federal University of Technology Akure"
-
-    return (
-        <>
-            <View 
-                center
-                style={{
-                    zIndex:1,
-                }}
-            >
-
-                <Image
-                    source={assets.user}
-                    resizeMode="cover"
-                    width={80} height={80}
-                    style={{
-                        position:'relative',
-                        bottom: -20,
-                        
-                    }}
-                />
-
-            </View>
-
-            <View
-                style={{
-                    paddingTop: 30,
-                    paddingBottom: 5,
-                    backgroundColor:Theme.grey101,
-                }}
-            >
-                <Text 
-                    center 
-                    h5
-                >{name}</Text>                
-
-                <View 
-                    style={{
-                        marginVertical: 10,
-                    }}
-                >
-                    <Text center label>{"IT intern"}</Text>
-                    <Text center label>{school}</Text>
-                </View>
-            </View>
-
-            <View
-                center
-            >
-                <View
-                    center
-                    style={{
-                        flexDirection:'row',
-                        backgroundColor:Theme.lightRed,
-                        maxWidth: "80%",
-                        paddingHorizontal: 20,
-                        paddingVertical:10,
-                        borderRadius: 6,
-                        marginVertical: 10,
-                    }}
-                >
-                    <Octicons name="link-external" size={15} color={Theme.red}/>
-
-                    <Text style={{marginLeft: 10, color:Theme.red}}>
-                        View CV
-                    </Text>
-                </View>
-                
-            </View>
-        </>
-    )
-}
-
-const Tab = ({text, onClick, active})=> (
-    <TouchableOpacity 
-        onPress={onClick}
-        activeOpacity={0.6}
-        center 
-        style={{
-            backgroundColor:active ? Theme.secondary : 'transparent',
-            borderRadius: 6,
-            paddingVertical: 8,
-            paddingHorizontal: 15,
-            marginHorizontal: 15,
-        }}
-    >
-        <Text label style={{color: active ? Theme.white : Theme.accent }}>{text}</Text>
-    </TouchableOpacity>
-)
+import DetailHeader from '../../components/student/Detail';
+import Tabs from '../../components/Tabs';
+import Info from '../../components/student/Info';
+import { WeeklyLogs } from '../../components/student/Log';
 
 const InternsDetailScreen = ({ route }) => {
     const { internId} = route.params;
     const internData = InternLists.find(each => each.id === internId);
-    const internJob = JobsLists.find(each => each.id === internData.jobId);
 
     const [tabNo, setTabNo] = useState(0);    
     const [weekEditing, setWeekEditing] = useState(null);
-
 
     const autoSaveLog = (data)=>{
 
@@ -119,6 +23,13 @@ const InternsDetailScreen = ({ route }) => {
     }
 
     if (!Boolean(internData)) return <JobNotFound/>;
+
+    const log = `Date: 1/1/2023
+
+A sample weekly log.
+
+supervisor: Mr Lorem Bulaba (Manager)
+`
 
     return (
         <>
@@ -128,7 +39,7 @@ const InternsDetailScreen = ({ route }) => {
                 }}
             >
 
-                <InternDetailHeader intern = { internData }/>
+                <DetailHeader data = { internData }/>
 
                 {/* Tabs */}
                 <View 
@@ -137,20 +48,20 @@ const InternsDetailScreen = ({ route }) => {
                         marginBottom: 10,
                     }}
                 >
-                    <View
-                        style={{
-                            flexDirection:'row', 
-                            alignItems:'center', 
-                            justifyContent:'space-evenly',
-                            backgroundColor:Theme.white,
-                            padding: 5,
-                            borderRadius: 5,
-                            // maxWidth: "80%"
-                        }}
-                    >
-                        <Tab text="Intern Info" onClick={()=>setTabNo(0)} active={tabNo === 0}/>
-                        <Tab text="Weekly Reviews" onClick={()=>setTabNo(1)} active={tabNo === 1}/>
-                    </View>
+                    <Tabs
+                        tabs={[
+                            {
+                                text: "Intern Info",
+                                onClick:()=>setTabNo(0),
+                                active: tabNo === 0
+                            },
+                            {
+                                text: "Weekly Reviews",
+                                onClick:()=>setTabNo(1),
+                                active: tabNo === 1
+                            }
+                        ]}
+                    />
                 </View>
             </View>
 
@@ -158,19 +69,20 @@ const InternsDetailScreen = ({ route }) => {
                 
             {
                 tabNo === 0 ? 
-                <InternInfo/>
+                <Info/>
                 :
-                <InternWeekelyReview onEditLog={(weekNumber)=>setWeekEditing(weekNumber)}/>
+                <WeeklyLogs onEditLog={(weekNumber)=>setWeekEditing(weekNumber)}/>
             }
 
-            <LogBottomSheet weekly={true} show={Boolean(weekEditing)} data={weekEditing} onDismiss={autoSaveLog}/>
+            <LogBottomSheet 
+                weekly={true} 
+                show={Boolean(weekEditing)} 
+                data={log} 
+                onDismiss={autoSaveLog}
+            />
         </>
 
     );
 }
 
 export default InternsDetailScreen;
-
-
-
-const styles = StyleSheet.create({})
