988c647dec93c200af94c9b816e61d4cfa42d30a
Precious Olusola <preciousolusola16@gmail.com>
2023-02-09 22:58:30 +0100

    Adding redux

---
+++ app/store/sampleSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+const USERS_URL = 'https://dummyjson.com/users';//'https://jsonplaceholder.typicode.com/users';
+
+// Empty arrays for users
+const initialState = {}
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    const response = await axios.get(USERS_URL);
+    return response.data;
+});
+
+
+const usersSlice = createSlice({
+    name: 'users',
+    initialState,
+    reducers: {
+        sampleReducer(state,action){
+            console.log(state);
+        },
+    },
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            return action.payload.users
+        });
+    }
+})
+
+export const selectAllUsers = (state) => state.users;
+export const selectUserById = (state, userId) => state.users.find(user => user.id === userId);
+
+export default usersSlice.reducer;
