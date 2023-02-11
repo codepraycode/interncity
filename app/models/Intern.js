6bebe8af8e0544f875caf47cbc2e8128b142c164
Precious Olusola <preciousolusola16@gmail.com>
2023-02-11 13:03:45 +0100

    Added the intern/application class

---
+++ app/models/Intern.js
@@ -0,0 +1,71 @@
+// User Data and User Account Model
+
+import { HandlerJoiError, JSONLog,} from "../utils";
+import { jobSchema } from "./base";
+import {
+  doc,
+  addDoc,
+  updateDoc,
+  deleteDoc
+} from 'firebase/firestore';
+import { collectionNames, database, jobsCollectionRef } from "../firebaseConfig";
+
+
+
+class Intern {
+    #original = undefined;
+
+
+    get original(){
+        return this.#original;
+    }
+
+    constructor(data){ // application data
+        
+        this.#original = data;
+        
+        const {
+            id,
+            job,
+            organization,
+            
+            student,
+            duration,
+
+            date_applied,
+            offer_date,
+            job_started,
+            job_ended,
+        } = data || {};
+
+        this.id = id;
+        this.jobId = job;
+        this.studentId = student;
+        this.organizationId = organization;
+
+        this.duration = duration;
+        this.date_applied = date_applied;
+        this.offer_date = offer_date;
+        this.job_started = job_started;
+        this.job_ended = job_ended;
+
+        this.job = null;
+        this.student = null;
+        this.organization = null;
+    }
+
+    setJob(jobInstance){
+        this.job = jobInstance;
+    }
+    setStudent(instance){
+        this.student = instance;
+    }
+    setOrganization(instance){
+        this.organization = instance;
+    }
+
+
+}
+
+
+export default Intern;
